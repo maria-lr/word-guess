e4d6602ba047837b3c6e890c1dd318072d20db82
Maria LaRocca <mclarocca@gmail.com>
2020-05-02 15:53:43 -0400

    Update css, chances decrement, correct letter-guessing

--- main.js
+++ main.js
@@ -1,143 +1,131 @@
-let wordToGuess = null;
-
-fetch('https://random-word-api.herokuapp.com/word?number=1')
-  .then((res) => res.json())
-  .then((result) => {
-    // console.log(result)
-    wordToGuess = result[0];
-
-
-
-
-
-  });
-
-// console.log('log after fetch request', wordToGuess);
-
-const button = document.getElementById("guess");
-
-
-
-button.onclick = function (event) {
-
-};
-
-// Accessible focus ring invisible until user tabs. Code from: https://medium.com/hackernoon/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2.
-function handleFirstTab(e) {
-  if (e.keyCode === 9) {
-    // the "I am a keyboard user" key
-    document.body.classList.add("user-is-tabbing");
-    window.removeEventListener("keydown", handleFirstTab);
-  }
-}
-
-window.addEventListener("keydown", handleFirstTab);
-
-
-
-
-function waitForStuff() {
-
-  console.log("1 We called our funtion!!")
-
-  fetch('https://random-word-api.herokuapp.com/word?number=1')
-    .then((res) => res.json())
-    .then((result) => {
-      console.log('2 Fetch has finished!!!')
-    });
-
-  console.log("3 This is after our fetch in the code")
-
-}
-
-async function waitForStuffFORREAL() {
-  console.log('1')
-  const result = await fetch('https://random-word-api.herokuapp.com/word?number=1');
-  const formattedResult = await result.json();
-  console.log('2 result is..', formattedResult)
-  console.log('3')
-
-}
-
-// waitForStuff();
-// waitForStuffFORREAL();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 async function startGame() {
   console.log("Game has started!!!")
 
   const result = await fetch('https://random-word-api.herokuapp.com/word?number=1');
   const formattedResult = await result.json();
   const word = formattedResult[0];
-
-  console.log('The word is:', word)
-
+  const guessButton = document.getElementById("guess");
+  const startOverButton = document.getElementById("reset");
+  let gameResultMessage = document.getElementById('game-result');
+  const lettersArea = document.getElementById("letters")
   const splitString = word.split("");
-  console.log(splitString);
-  const numberOfLetters = word.length;
-  document.getElementById("letter-quantity").innerText = numberOfLetters;
-
-  // document.getElementById("word").innerText = word;
-
   let numberOfChances = 10;
   document.getElementById("chances").innerText = numberOfChances;
 
-  const guessButton = document.getElementById("guess");
-  const startOverButton = document.getElementById("reset");
-  const letterGuessed = document.getElementById("letter-guess").innerText;
+  console.log('The word is:', word)
+  console.log(splitString);
 
   function createLetterSpace() {
-    let letterSpace = document.createElement("div");
-    letterSpace.style.border = "1px solid black";
-    letterSpace.style.height = "100px";
-    letterSpace.style.width = "100px";
-    letterSpace.style.display = "inline-block";
-    document.getElementById("letters").appendChild(letterSpace);
-
-  }
+    let newLetterSpace = document.createElement("div");
+    newLetterSpace.style.borderBottom = "1px solid black";
+    newLetterSpace.style.maxHeight = "100px";
+    newLetterSpace.style.minHeight = "80px";
+    newLetterSpace.style.maxWidth = "100px";
+    newLetterSpace.style.minWidth = "80px";
+    newLetterSpace.style.display = "inline-block";
+    newLetterSpace.style.margin = "5px";
+    lettersArea.appendChild(newLetterSpace)
+  };
 
   splitString.forEach(createLetterSpace);
 
-
-  // letterSpace.id = "letters-container"
-
-
-
-
+  // When you click the guess button...
   guessButton.onclick = function (event) {
-    if (word.includes(letterGuessed)) {
 
-    }
-    else {
+    const letterGuessed = document.getElementById("letter-guess").value;
+    const allLetters = document.querySelectorAll('#letters div');
+    document.getElementById("chances").innerText = numberOfChances;
+
+    if (word.includes('letterGuessed') === false) {
       numberOfChances--
-    }
+      document.getElementById("chances").innerText = numberOfChances;
+    };
+
+    //Check if the letter guessed matches a letter in the mystery word.
+    allLetters.forEach(function (div, index) {
+      if (letterGuessed === splitString[index]) {
+        div.innerText = letterGuessed;
+      };
+      // document.getElementById("chances").innerText = numberOfChances;
+      // else {
+      //   numberOfChances--
+      // };
+
+
+      // allLetters.every(function (div, string) {
+      //   div.includes(string)
+      // })
+
+    });
+    // if (word.includes('letterGuessed') === false) {
+    //   numberOfChances--
+    //   document.getElementById("chances").innerText = numberOfChances;
+    // };
+
+
+    function winLoseMessage() {
+      const arrayOfAllLetters = Array.prototype.slice.call(allLetters);
+
+      // console.log('all letters is...', arrayOfAllLetters)
+
+
+      const allSpacesFull = arrayOfAllLetters.every(function (div, string) {
+        div.innerText.includes(string)
+      });
+      console.log(allSpacesFull);
+
+      if (numberOfChances === 0) {
+        gameResultMessage.innerText = 'You lose.'
+      } else if (numberOfChances > 0 && allSpacesFull) {
+        gameResultMessage.innerText = 'You win!'
+      }
+    };
+
+    winLoseMessage();
   };
+
+  // function winLoseMessage() {
+  //   if (numberOfChances === 0) {
+  //     gameResultMessage.innerText = 'You lose.'
+  //   } else if (numberOfChances > 0 &&  != "")
+  //     gameResultMessage.innerText = 'You win!'
+
+  // };
+
+  // winLoseMessage();
 
   startOverButton.onclick = function (event) {
     location.reload();
   };
 
-  function startOver() { }
-}
+
+};
 
 startGame();
+
+// const numberOfLetters = word.length;
+// document.getElementById("letter-quantity").innerText = numberOfLetters;
+// document.getElementById("word").innerText = word;
+
+
+// letterSpace.id = "letters-container"
+
+// function startOver() { }
+
+// function doesLetterMatch(){
+
+;
+
+// }
+
+
+      // if (lettersArea.includes(letterGuessed)) {
+      //   lettersArea.style.display = "inline-block";
+
+      // }
+      // if (word.includes(letterGuessed)) {
+
+      // allLetters.forEach()
+      // lettersArea.innerText = letterGuessed;
+
+
